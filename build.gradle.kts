bae0e9552028ba78cf7c25a24e4c298545804938
404Setup <153366651+404Setup@users.noreply.github.com>
2024-11-11 08:35:35 +0800

    [skip ci] Bump Dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -46,6 +46,8 @@ subprojects {
         mavenCentral()
         maven(paperMavenPublicUrl)
         maven("https://oss.sonatype.org/content/groups/public/")
+        maven("https://oss.sonatype.org/content/repositories/snapshots")
+        maven("https://maven.repository.redhat.com/ga/")
         maven("https://ci.emc.gs/nexus/content/groups/aikar/")
         maven("https://repo.aikar.co/content/groups/aikar")
         maven("https://repo.md-5.net/content/repositories/releases/")
