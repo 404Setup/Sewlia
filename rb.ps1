2c9ea1eaaa7af32d34423324aef643bb86988698
404Setup <153366651+404Setup@users.noreply.github.com>
2025-01-27 21:50:13 +0800

    [skip ci] update

--- rb.ps1
+++ rb.ps1
@@ -1,3 +1,6 @@
+./gradlew fixupPaperApiFilePatches
+./gradlew fixupPaperServerFilePatches
+./gradlew fixupMinecraftSourcePatches
 ./gradlew rebuildFoliaPatches || exit_on_error "An error occurred when rebuilding patches!"
 ./gradlew rebuildServerPatches || exit_on_error "An error occurred when rebuilding patches!"
 ./gradlew rebuildMinecraftPatches || exit_on_error "An error occurred when rebuilding patches!"
