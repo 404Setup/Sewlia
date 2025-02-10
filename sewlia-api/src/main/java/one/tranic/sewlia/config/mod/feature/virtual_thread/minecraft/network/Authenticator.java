8e063b3422ab7c471270d4d2268309575637292e
404Setup <153366651+404Setup@users.noreply.github.com>
2025-02-10 22:52:58 +0800

    [skip ci] Continue to migrate and fix bugs

---
+++ sewlia-api/src/main/java/one/tranic/sewlia/config/mod/feature/virtual_thread/minecraft/network/Authenticator.java
@@ -0,0 +1,8 @@
+package one.tranic.sewlia.config.mod.feature.virtual_thread.minecraft.network;
+
+import one.tranic.sewlia.config.annotation.DisableReload;
+
+public class Authenticator {
+    @DisableReload
+    public static boolean value = false;
+}
