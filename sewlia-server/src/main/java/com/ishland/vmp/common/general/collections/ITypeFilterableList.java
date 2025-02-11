d1700f6f3a9656df2751e148f323b5b16c604644
404Setup <153366651+404Setup@users.noreply.github.com>
2025-02-11 12:32:54 +0800

    [skip ci] next

---
+++ sewlia-server/src/main/java/com/ishland/vmp/common/general/collections/ITypeFilterableList.java
@@ -0,0 +1,8 @@
+package com.ishland.vmp.common.general.collections;
+
+// Source in https://github.com/RelativityMC/VMP-fabric
+// com.ishland.vmp.common.general.collections.ITypeFilterableList;
+
+public interface ITypeFilterableList {
+    Object[] getBackingArray();
+}
