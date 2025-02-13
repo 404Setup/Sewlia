f375a0c1ebed39465b7c995fa7f7098a9db085f9
404Setup <153366651+404Setup@users.noreply.github.com>
2025-02-13 15:00:17 +0800

    Fix Spark not reading Sewlia configuration file

--- sewlia-server/src/main/java/one/tranic/sewlia/preload/mod/Spark.java
+++ sewlia-server/src/main/java/one/tranic/sewlia/preload/mod/Spark.java
@@ -5,6 +5,6 @@ import one.tranic.sewlia.preload.Preloads;
 public class Spark extends Preloads {
     @Override
     public void doPreload() {
-        System.setProperty("spark.serverconfigs.extra", "sewlia.yml");
+        System.setProperty("spark.serverconfigs.extra", "sewlia.yaml");
     }
 }
