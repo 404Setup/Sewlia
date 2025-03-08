64876aee5da4b2233701ba2f71cf6faa0b061f9c
404Setup <153366651+404Setup@users.noreply.github.com>
2025-03-08 23:39:44 +0800

    bump version

--- sewlia-api/src/main/java/one/tranic/sewlia/config/mod/fix/OldPosition.java
+++
@@ -1,9 +0,0 @@
-package one.tranic.sewlia.config.mod.fix;
-
-import one.tranic.sewlia.config.annotation.Comments;
-
-public class OldPosition {
-    @Comments({"A temporary fix of these issues:" +
-            "https://github.com/PaperMC/Folia/issues/311"})
-    public static boolean value = true;
-}
