198d3ec014e3d7320e823110c333b0384ec2951e
404Setup <153366651+404Setup@users.noreply.github.com>
2025-03-15 12:28:30 +0800

    bump version

---
+++ sewlia-api/src/main/java/one/tranic/sewlia/config/mod/unsafe/raid/SkipHeightCheck.java
@@ -0,0 +1,5 @@
+package one.tranic.sewlia.config.mod.unsafe.raid;
+
+public class SkipHeightCheck {
+    public static boolean value = false;
+}
---
+++ sewlia-api/src/main/java/one/tranic/sewlia/config/mod/unsafe/raid/SkipSelfRaidCheck.java
@@ -0,0 +1,5 @@
+package one.tranic.sewlia.config.mod.unsafe.raid;
+
+public class SkipSelfRaidCheck {
+    public static boolean value = false;
+}
