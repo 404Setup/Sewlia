83690542d9c83545cc672e7948fed10f83eb8d19
404Setup <153366651+404Setup@users.noreply.github.com>
2025-03-16 16:49:28 +0800

    Compensate for missing configuration classes

---
+++ sewlia-api/src/main/java/one/tranic/sewlia/config/mod/unsafe/fix/force_cleanup_brain_memory/EnabledForBlockPos.java
@@ -0,0 +1,5 @@
+package one.tranic.sewlia.config.mod.unsafe.fix.force_cleanup_brain_memory;
+
+public class EnabledForBlockPos {
+    public static boolean value = false;
+}
---
+++ sewlia-api/src/main/java/one/tranic/sewlia/config/mod/unsafe/fix/force_cleanup_brain_memory/EnabledForEntity.java
@@ -0,0 +1,5 @@
+package one.tranic.sewlia.config.mod.unsafe.fix.force_cleanup_brain_memory;
+
+public class EnabledForEntity {
+    public static boolean value = false;
+}
