5abc2021353769cd46cfe2bb43186d5f47002be1
404Setup <153366651+404Setup@users.noreply.github.com>
2025-03-28 16:21:58 +0800

    Fix the wrong log

--- sewlia-api/src/main/java/one/tranic/sewlia/config/mod/unsafe/optimization/fast_rng/WarnForSlimeChunk.java
+++ sewlia-api/src/main/java/one/tranic/sewlia/config/mod/unsafe/optimization/fast_rng/WarnForSlimeChunk.java
@@ -12,10 +12,8 @@ public class WarnForSlimeChunk {
             ConfigUtils.logger.warn("==========");
             ConfigUtils.logger.warn("You enabled faster random generator, it will offset location of slime chunk");
             ConfigUtils.logger.warn("If your server has slime farms or facilities need vanilla slime chunk,");
-            ConfigUtils.logger.warn("set performance.faster-random-generator.use-legacy-random-for-slime-chunk " +
-                    "to true to use LegacyRandomSource for slime chunk generation.");
-            ConfigUtils.logger.warn("Set performance.faster-random-generator.warn-for-slime-chunk to false to " +
-                    "disable this warning.");
+            ConfigUtils.logger.warn("set unsafe.faster-rng.use-legacy-random-for-slime-chunk to true to use LegacyRandomSource for slime chunk generation.");
+            ConfigUtils.logger.warn("Set unsafe.faster-rng.warn-for-slime-chunk to false to disable this warning.");
             ConfigUtils.logger.warn("==========");
         }
     }
